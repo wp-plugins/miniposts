47bf7656e4152434d005742e13b83fce1ea3ee8b
erigami <erigami@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-20 04:26:53 +0000

    Initial sane checking. Corresponds to version 0.6.8.
    git-svn-id: https://plugins.svn.wordpress.org/miniposts/trunk@81295 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ options.php
@@ -0,0 +1,156 @@
+<?php
+include(ABSPATH.'wp-blog-header.php');
+
+$paged_val = get_query_var('paged');
+$paged_val = $paged_val ? "&paged=$paged_val" : '';
+
+if (isset($_POST["update_options"])) {
+    $errors = array();
+
+    update_option('filter_mini_posts_from_loop', $_POST['filter_mini_posts_from_loop'] == 1 ? 1 : 0);
+
+    update_option('suppress_autop_on_mini_posts', $_POST['suppress_autop_on_mini_posts'] == 1 ? 1 : 0);
+
+    update_option('filter_mini_posts_from_feeds', $_POST['filter_mini_posts_from_feeds'] == 1 ? 1 : 0);
+    
+    if (ctype_digit($_POST['miniposts_maximum'])) {
+        update_option('miniposts_maximum', $_POST['miniposts_maximum']);
+    }
+    else {
+        $errors[] = __("Maximum number of miniposts must be a number.");
+    }
+    
+    update_option('miniposts_format', stripslashes($_POST['miniposts_format']));
+    
+    update_option('miniposts_date_format', stripslashes($_POST['miniposts_date_format']));
+    
+    update_option('miniposts_more_text', stripslashes($_POST['miniposts_more_text']));
+
+    update_option('miniposts_title', stripslashes($_POST['miniposts_title']));
+
+    if (sizeof($errors) == 0) {
+        echo '<div class="updated"><p><strong>' . __('Options saved.', 'MiniPosts') . '</strong></p></div>';
+    } else {
+        echo '<div class="error"><p><strong>';
+        echo __('Options partially saved. Error in the data submitted: ', 'MiniPosts');
+        foreach ($errors as $e) {
+            echo '<li>';
+            echo $e;
+            echo "</li>\n";
+        }
+        echo '</strong></p></div>';
+    }
+}
+
+$ck_filter_mini_posts_from_loop = get_settings('filter_mini_posts_from_loop') == 1 ? 'checked="checked" ' : '';
+$ck_suppress_autop_on_mini_posts = get_settings('suppress_autop_on_mini_posts') == 1 ? 'checked="checked" ' : '';
+$ck_filter_mini_posts_from_feeds = get_settings('filter_mini_posts_from_feeds') == 1 ? 'checked="checked" ' : '';
+
+?>
+<div class="wrap">
+<div id="icon-options-general" class="icon32">
+    <br/>
+</div>
+<h2><?php _e('Minipost Options', 'MiniPosts') ?></h2>
+<form method="post" action="./options-general.php?page=<?php echo 'miniposts/' . basename(__FILE__) . $paged_val; ?>">
+    <table width="100%" cellspacing="2" cellpadding="5" class="form-table">
+        <tr valign="top">
+            <th scope="row"><?php _e('Filtering', 'MiniPost') ?></th>
+            <td>
+                <label for="filter_mini_posts_from_loop"><input type="checkbox" name="filter_mini_posts_from_loop" id="filter_mini_posts_from_loop" <?php echo $ck_filter_mini_posts_from_loop ?> value="1" /> <?php _e('Hide miniposts from the Loop', 'MiniPosts') ?></label><br />
+                <label for="filter_mini_posts_from_feeds"><input type="checkbox" name="filter_mini_posts_from_feeds" id="filter_mini_posts_from_feeds" <?php echo $ck_filter_mini_posts_from_feeds ?> value="1" /> <?php _e('Hide miniposts from subscription feeds', 'MiniPosts') ?></label><br />
+                <p/>
+                <label for="miniposts_maximum"><input type="text" name="miniposts_maximum" id="miniposts_maximum" size="2" value="<?php echo get_option('miniposts_maximum')?>"/> <?php _e('Maximum number of miniposts to display. 0 (default) displays all miniposts', 'MiniPosts') ?></label> 
+            </td>
+        </tr>
+
+        <tr valign="top">
+            <th scope="row"><?php _e('Auto-paragraphing:', 'MiniPosts') ?></th>
+            <td><label for="suppress_autop_on_mini_posts"><input type="checkbox" name="suppress_autop_on_mini_posts" id="suppress_autop_on_mini_posts" <?php echo $ck_suppress_autop_on_mini_posts ?> value="1" /> <?php _e('Suppress auto-paragraphing on mini posts in the Loop', 'MiniPosts') ?></label></td>
+        </tr>
+
+        <tr valign="top">
+            <th width="25%" scope="row"><?php _e('Widget title', 'MiniPosts') ?></th>
+            <td>
+                <input type="text" name="miniposts_title" value="<?php 
+                    echo htmlspecialchars(get_option("miniposts_title"));
+                    ?>" size="60">
+                    <div class="miniposts_format_options">
+                        The name to display at the top of the miniposts 
+                        widget.
+                    </div>
+            </td>
+        </tr>
+            
+        <tr valign="top">
+            <th scope="row"><?php _e('Minipost appearance', 'MiniPosts') ?></th>
+            <td><label for="suppress_autop_on_mini_posts"><input type="checkbox" name="suppress_autop_on_mini_posts" id="suppress_autop_on_mini_posts" <?php echo $ck_suppress_autop_on_mini_posts ?> value="1" /> <?php _e('Prevent automatic paragraph breaks in miniposts', 'MiniPosts') ?></label>
+
+                <p/>
+                Control the appearance of each minipost by editing the display
+                format.
+
+                <div style="padding-left: 3ex;">
+                <textarea name="miniposts_format" cols="80"><?php 
+                    echo htmlspecialchars(get_option("miniposts_format"));
+                ?></textarea>
+                <br>
+                <div class="miniposts_format_options" style="padding-left: 3ex;">
+                    <b>%date%</b> - The date the minipost was 
+                            posted. <br/>
+                    <b>%title%</b> - The title of the minipost<br/>
+                    <b>%post%</b> - The text of the post<br/>
+                    <b>%commentcount%</b> - The number of comments for the
+                    post<br/>
+                    <b>%permalink%</b> - The permalink to the post<br/>
+                    <b>%more%</b> - Text to be displayed if the post has 
+                    more content than shown. 
+                </div>
+                </div>
+            </td>
+        </tr>
+
+        <tr valign="top">
+            <th scope="row"><?php _e('More appearance', 'MiniPosts') ?></th>
+            <td>
+                <input type="text" name="miniposts_more_text" value="<?php 
+                    echo htmlspecialchars(get_option("miniposts_more_text"));
+                    ?>" size="60">
+                <br>
+                <div class="miniposts_format_options">
+                    The format of the text to be shown in place of the 
+                    <tt>%more%</tt> keyword listed above. This keyword is 
+                    shown if you use the <i>Optional Excerpt</i> or 
+                    <tt>&lt;!--more--&gt;</tt> in your post. You may use 
+                    any other keyword in this text. 
+                </div>
+            </td>
+        </tr>
+
+        <tr valign="top">
+            <th scope="row"><?php _e('Date Format', 'MiniPosts') ?></th>
+            <td>
+                <input type="text" name="miniposts_date_format" value="<?php 
+                    echo htmlspecialchars(get_option("miniposts_date_format"));
+                    ?>">
+                <br>
+                <div class="miniposts_format_options">
+                            The format of the date to display.
+                            To set your own, see the list of 
+                            <a href="http://ca.php.net/manual/en/function.date.php">standard PHP 
+                            <code>date()</code> arguments</a>.
+                </div>
+            </td>
+        </tr>
+    </table>
+    <div class="submit"><input type="submit" name="update_options" value="<?php _e('Update Options', 'MiniPosts') ?> &raquo;" /></div>
+</form>
+</div>
+<?php
+
+if ($_GET["miniposts_updated"] == "true"):
+?>
+    <div class="updated"><p><strong><?php _e('Mini posts updated.', 'MiniPosts') ?></strong></p></div>
+<?php
+endif;
+?>
---
+++ readme.txt
@@ -0,0 +1,39 @@
+=== Plugin Name ===
+Contributors: erigami
+Tags: aside, minipost, linkblog, widget, twitter
+Requires at least: 2.0.2
+Tested up to: 2.7
+Stable tag: 0.6.8
+
+Create small posts outside the main flow of your blog. Integrates into any theme that supports widgets.
+
+
+== Description ==
+
+This plugin allows you to mark any post as a minipost. Miniposts are shown 
+in a widget on your main page (or elsewhere). Features:
+
+* Integrates easily into any theme that supports widgets
+* Complete control of minipost appearance. Show the title, text, comment count, post date, permalink, and custom 'more' text. 
+* Miniposts will show a teaser if the post has an excerpt or a <code>&lt;!--more--&gt;</code> tag.
+* API to integrate minipost display into non-widgetty themes
+
+
+== Installation ==
+
+1. Download and unzip. 
+1. Copy the miniposts directory into your [wordpress-install]/wp-content/plugins directory.
+1. Activate the "Miniposts" plugin in your administrator panel.
+1. Add the "Miniposts" widget to your theme's sidebar.
+
+
+== Frequently Asked Questions ==
+
+= What is the non-widget API? =
+
+MiniPosts2 provides two functions, <code>is_mini_post()</code>, and <code>get_mini_posts()</code>. 
+
+**<code>is_mini_post()</code>** - This function takes no arguments and can only be called from within the Loop. Returns *true* if the current post is a minipost, and *false* otherwise. 
+
+**<code>get_mini_posts($format = null, $limit = null)</code>** - This function displays the existing miniposts. If either of the arguments are specified as non-null then the display is changed. The *$format* variable is used to specify a format, over-riding the values specified on the Options page.
+
